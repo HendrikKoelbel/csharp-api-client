38fd823d994339b23c83a901bb923b5a4ceec752
bbuendia <brendab@mifiel.com>
2019-02-12 12:20:21 -0600

    Include pkcs5 feature

---
+++ MifielAPI/MifielAPI/Crypto/Pkcs5.cs
@@ -0,0 +1,129 @@
+﻿namespace MifielAPI.Crypto
+{
+    using MifielAPI.Exceptions;
+    using Org.BouncyCastle.Asn1;
+    using Org.BouncyCastle.Asn1.Nist;
+    using Org.BouncyCastle.Asn1.Pkcs;
+    using Org.BouncyCastle.Asn1.X509;
+    using System;
+
+    public class Pkcs5
+    {
+        public byte[] Salt { get; set; }
+        public int Iterations { get; set; }
+        public byte[] Iv { get; set; }
+        public byte[] Encrypted { get; set; }
+        public int SizeKey { get; set; }
+
+        public Pkcs5()
+        {
+
+        }
+
+        public Pkcs5(byte[] salt, byte[]iv, byte[] encrypted, int iterations, int sizekey)
+        {
+            Salt = salt;
+            Iv = iv;
+            Encrypted = encrypted;
+            Iterations = iterations;
+            SizeKey = sizekey;
+        }
+
+        private KeyDerivationFunc GeneratePkbdAlgorithmIdentifier(byte[] pbkdSalt, int iterations)
+        {
+            return new KeyDerivationFunc(PkcsObjectIdentifiers.IdPbkdf2, new Pbkdf2Params(pbkdSalt, iterations,
+                    new AlgorithmIdentifier(PkcsObjectIdentifiers.IdHmacWithSha256)));
+        }
+
+        public byte[] Create()
+        {
+
+            ValidateData();
+            KeyDerivationFunc pbkdAlgId = GeneratePkbdAlgorithmIdentifier(Salt, Iterations);
+            DerObjectIdentifier id = null;
+            switch (SizeKey)
+            {
+                case 16:
+                    id = NistObjectIdentifiers.IdAes128Cbc;
+                    break;
+                case 24:
+                    id = NistObjectIdentifiers.IdAes192Cbc;
+                    break;
+                case 32:
+                    id = NistObjectIdentifiers.IdAes256Cbc;
+                    break;
+                default:
+                    throw new MifielException("Key length not 128/192/256 bits");
+            }
+
+            PbeS2Parameters pbeParams = new PbeS2Parameters(pbkdAlgId,
+                
+                    new EncryptionScheme(id, new DerOctetString(Iv)));
+            EncryptedPrivateKeyInfo keyInfo = new EncryptedPrivateKeyInfo(
+                    new AlgorithmIdentifier(PkcsObjectIdentifiers.IdPbeS2, pbeParams), Encrypted);
+            return keyInfo.GetEncoded();
+        }
+
+        private void ValidateData()
+        {
+            if (Salt == null)
+                throw new MifielException("SALT cannot be null");
+
+            if (Iv == null)
+                throw new MifielException("IV cannot be null");
+
+            if (Encrypted == null)
+                throw new MifielException("Data ENCRYPTED cannot be null");
+
+        }
+
+        public void Read(byte[] pkcs5Bytes)
+        {
+            EncryptedPrivateKeyInfo encPkInfo = null;
+            Console.WriteLine(pkcs5Bytes.Length);
+            try
+            {
+                encPkInfo = EncryptedPrivateKeyInfo.GetInstance(Asn1Sequence.GetInstance(pkcs5Bytes));
+            }
+            catch (Exception e1)
+            {
+                throw new MifielException("Exception decoding bytes: Bytes are not PKCS5");
+            }
+
+            Encrypted = encPkInfo.GetEncryptedData();
+            PbeS2Parameters alg = PbeS2Parameters.GetInstance(encPkInfo.EncryptionAlgorithm.Parameters);
+            Pbkdf2Params func = Pbkdf2Params.GetInstance(alg.KeyDerivationFunc.Parameters);
+            EncryptionScheme scheme = alg.EncryptionScheme;
+
+            if (!PkcsObjectIdentifiers.IdHmacWithSha256.Equals(func.Prf.Algorithm))
+            {
+                throw new MifielException("Digest algorithm not supported");
+            }
+
+            if (!(NistObjectIdentifiers.IdAes128Cbc.Equals(scheme.Algorithm)
+                    || NistObjectIdentifiers.IdAes192Cbc.Equals(scheme.Algorithm)
+                    || NistObjectIdentifiers.IdAes256Cbc.Equals(scheme.Algorithm)))
+            {
+                throw new MifielException("Encryption algorithm not supported");
+            }
+
+            SetSizeKey(scheme.Algorithm);
+            Iterations = func.IterationCount.IntValue;
+            Salt = func.GetSalt(); 
+            Iv = Asn1OctetString.GetInstance(scheme.Parameters).GetOctets();
+        }
+
+
+        private void SetSizeKey(DerObjectIdentifier algorithm)
+        {
+            if (NistObjectIdentifiers.IdAes128Cbc.Equals(algorithm))
+                SizeKey = 16;
+
+            if (NistObjectIdentifiers.IdAes192Cbc.Equals(algorithm))
+                SizeKey = 24;
+
+            if (NistObjectIdentifiers.IdAes256Cbc.Equals(algorithm))
+                SizeKey = 32;
+        }
+    }
+}
---
+++ MifielAPI/MifielApiTests/crypto/Pkcs5Test.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using MifielAPI.Crypto;
+using MifielAPITests.crypto;
+using MifielAPI.Exceptions;
+
+namespace MifielAPITests
+{
+    [TestFixture]
+    public class Pkcs5Test : CryptoTest<ItemPkcs5>
+    {
+        private static readonly string fixturePath = "crypto\\fixturePKCS5.json";
+
+        public Pkcs5Test() : base(fixturePath)
+        {
+        }
+
+        [Test]
+        public void PKCS5_Read()
+        {
+            foreach (ItemPkcs5 item in ArrayTest.GetRange(0, 3))
+            {
+                Pkcs5 pkcs5 = new Pkcs5();
+                pkcs5.Read(StringToByteArray(item.pkcs5));
+                Assert.AreEqual(item.salt, BitConverter.ToString(pkcs5.Salt).Replace("-", "").ToLower());
+                Assert.AreEqual(item.iterations, pkcs5.Iterations);
+                Assert.AreEqual(item.iv, BitConverter.ToString(pkcs5.Iv).Replace("-", "").ToLower());
+                Assert.AreEqual(item.encrypted, BitConverter.ToString(pkcs5.Encrypted).Replace("-", "").ToLower());
+                Assert.AreEqual(item.sizeKey, pkcs5.SizeKey);
+            }
+        }
+
+        [Test]
+        public void PKCS5_Create()
+        {
+            foreach (ItemPkcs5 item in ArrayTest.GetRange(0, 3))
+            {
+                Pkcs5 pkcs5 = new Pkcs5(StringToByteArray(item.salt), StringToByteArray(item.iv), StringToByteArray(item.encrypted), item.iterations, item.sizeKey);
+                byte[] res = pkcs5.Create();
+                Console.WriteLine(item.pkcs5);
+                Assert.AreEqual(item.pkcs5, BitConverter.ToString(res).Replace("-", "").ToLower());
+            }
+        }
+        [Test]
+        public void PKCS5_Exception()
+        {
+                Pkcs5 pkcs5 = new Pkcs5();
+                Assert.Throws<MifielException>(() => pkcs5.Read(StringToByteArray(ArrayTest[3].pkcs5)));
+                Assert.Throws<MifielException>(() => pkcs5.Read(StringToByteArray(ArrayTest[4].pkcs5)));
+                Assert.Throws<MifielException>(() => pkcs5.Read(StringToByteArray(ArrayTest[5].pkcs5)));
+        }
+
+
+    }
+
+    public class ItemPkcs5
+    {
+        public string pkcs5;
+        public string iv;
+        public string salt;
+        public int iterations;
+        public int sizeKey;
+        public string encrypted;
+    }
+}
